e915200e428bf6288d5b84ebabd4dfd67a162871
C Yuva Teja <yuvatejac123@gmail.com>
2026-06-27 15:12:38 +0530

    Add files via upload

---
+++ Backend/embeddings_service.py
@@ -0,0 +1,20 @@
+import google.generativeai as genai  # type: ignore[import]
+import os
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+MODEL = "models/embedding-001"
+
+def generate_embeddings(texts):
+    embeddings = []
+
+    for text in texts:
+        response = genai.embed_content(
+            model=MODEL,
+            content=text,
+            task_type="retrieval_document"
+        )
+
+        embeddings.append(response["embedding"])
+
+    return embeddings
